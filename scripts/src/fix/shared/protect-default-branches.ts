3d2d3f03a4b663b499ebe3339a73ab85553f52c0
galargh <piotr.galar@gmail.com>
2022-08-17 12:48:25 +0100

    chore: rebase on new abstractions

--- scripts/src/fix/shared/protect-default-branches.ts
+++ scripts/src/fix/shared/protect-default-branches.ts
@@ -1,66 +1,27 @@
-import * as terraform from '../../terraform'
-import * as yaml from '../../yaml'
-import YAML from 'yaml'
+import {Config} from '../../yaml/config'
+import {Repository, Visibility} from '../../resources/repository'
+import {RepositoryBranchProtectionRule} from '../../resources/repository-branch-protection-rule'
 
-// TODO: run this script as part of some workflow (PR?, sync?), for now run:
-//       npm run build && TF_WORKSPACE=ipfs node lib/actions/protect-default-branches.js
 export async function protectDefaultBranches(
   includePrivate: boolean = false
 ): Promise<void> {
-  const config = yaml.getConfig()
+  const config = Config.FromPath()
 
-  // TODO: we need a better abstraction for things that are defined in the YAML
-  //       ideally, we should be able to replace all of this with something like:
-  //       config.getRepositories()
-  const repositories = config.getResources([terraform.GithubRepository])
+  const repositories = config.getResources(Repository)
 
   for (const repository of repositories) {
-    // TODO: we need a better abstraction for things that are defined in the YAML
-    //       ideally, we should be able to replace all of this with something like:
-    //       const name = repository.getName()
-    //       const pattern = repository.getPattern()
-    const name = ((repository.value as YAML.Pair).key as YAML.Scalar<string>)
-      .value
-    const pattern =
-      (
-        ((repository.value as YAML.Pair).value as YAML.YAMLMap).items.find(
-          pair => {
-            return (pair.key as YAML.Scalar<string>).value === 'default_branch'
-          }
-        )?.value as YAML.Scalar<string>
-      )?.value || 'main'
-    const visibility =
-      (
-        ((repository.value as YAML.Pair).value as YAML.YAMLMap).items.find(
-          pair => {
-            return (pair.key as YAML.Scalar<string>).value === 'visibility'
-          }
-        )?.value as YAML.Scalar<string>
-      )?.value || 'public'
-
-    if (!includePrivate && visibility === 'private') {
-      continue
+    if (includePrivate || repository.visibility !== Visibility.Private) {
+      const rule = new RepositoryBranchProtectionRule(
+        repository.name,
+        repository.default_branch ?? 'main'
+      )
+      if (!config.someResource(rule)) {
+        console.log(
+          `Adding branch protection rule for ${rule.pattern} to ${rule.repository} repository`
+        )
+        config.addResource(rule)
+      }
     }
-
-    // TODO: we need a better abstraction here, one which has a proper constructor
-    //       and doesn't know anything about terraform (i.e. values.id)
-    //       ideally, we should be able to replace all of this with something like:
-    //       const rule = new BranchProtectionRule(name, pattern)
-    const rule = new terraform.GithubBranchProtection()
-    rule.index = `${name}:${pattern}`
-    rule.values = {
-      id: '',
-      repository: name,
-      pattern: pattern,
-      required_pull_request_reviews: [],
-      required_status_checks: []
-    }
-
-    // TODO: we should be able to get a YAML representation of a resource without providing a terraform context
-    const resource = await rule.getYAMLResource(null as any)
-
-    // add is a noop if a resource already exists
-    config.add(resource)
   }
 
   config.save()
