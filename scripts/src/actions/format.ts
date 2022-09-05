b96ddd5faf47a3a7060733aa3940d9fa6a6a3909
galargh <piotr.galar@gmail.com>
2022-09-05 16:05:30 +0200

    chore: move maintenance scripts

--- scripts/src/actions/format.ts
+++
@@ -1,9 +0,0 @@
-import 'reflect-metadata'
-import {Config} from '../yaml/config'
-
-async function run(): Promise<void> {
-  const config = Config.FromPath()
-  config.save()
-}
-
-run()
---
+++ scripts/src/actions/local/format.ts
@@ -0,0 +1,4 @@
+import 'reflect-metadata'
+import {format} from '../shared/format'
+
+format()
