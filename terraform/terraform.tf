2e25655dc0c7ae5a4383ddf6c62fa8882166109a
galargh <piotr.galar@gmail.com>
2023-05-19 15:25:51 +0200

    set up tf provider fork

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     github = {
-      source  = "integrations/github"
-      version = "5.25.0"
+      source  = "github.com/galargh/terraform-provider-github"
+      version = "5.25.2-rc1"
     }
   }
 
