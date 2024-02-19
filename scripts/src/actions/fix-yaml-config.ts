75f3f38e990286e9259d18e1e62cd2bffdfb7865
galargh <piotr.galar@gmail.com>
2024-02-19 12:57:05 +0000

    feat: use diff in the summary

--- scripts/src/actions/fix-yaml-config.ts
+++ scripts/src/actions/fix-yaml-config.ts
@@ -1,16 +1,24 @@
 import 'reflect-metadata'
 
 import { toggleArchivedRepos } from './shared/toggle-archived-repos'
-import { getAccessSummaryDescription } from './shared/get-access-summary-description'
+import { describeAccessChanges } from './shared/describe-access-changes'
 
 import * as core from '@actions/core'
 
 async function run(): Promise<void> {
   await toggleArchivedRepos()
 
-  const accessSummaryDescription = await getAccessSummaryDescription()
+  const accessChangesDescription = await describeAccessChanges()
 
-  core.setOutput('comment', accessSummaryDescription)
+  core.setOutput('comment', `The following access changes will be introduced as a result of applying the plan:
+
+<details><summary>Access Changes</summary>
+
+\`\`\`
+${accessChangesDescription}
+\`\`\`
+
+</details>`)
 }
 
 run()
